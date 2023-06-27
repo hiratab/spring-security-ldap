763c0e4a289cf4cbc081e9ba6ed727b41002ef94
Bruno Hirata <brn.hirata@gmail.com>
2023-06-27 15:37:40 +0100

    feat: 🎸 Initial commit
    \

---
+++ src/test/java/com/hiratab/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiratab.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
